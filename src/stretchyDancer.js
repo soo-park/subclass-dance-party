e2505d58983c22eb645febdd97b7102aaae3da5a
Hack Reactor Student <student@hackreactor.com>
2017-05-23 17:30:00 -0700

    image stretches with resize

--- src/stretchyDancer.js
+++ src/stretchyDancer.js
@@ -1,7 +1,6 @@
 var StretchyDancer = function(top, left, timeBetweenSteps) {
+  var timeBetweenSteps = 10;  
   Dancer.call(this, top, left, timeBetweenSteps);
-
-  
 
 };
 
@@ -11,9 +10,9 @@ StretchyDancer.prototype.constructor = StretchyDancer;
 StretchyDancer.prototype.step = function() {
   this.oldStep = Dancer.prototype.step;
   this.oldStep();
-
+  console.log(this.$node);
   this.$node.animate({
-    height: $("body").height() * Math.random() / 20,
-    width: $("body").width() * Math.random() / 20
+    height: $("img").height() * Math.random(),
+    width: $("img").width() * Math.random()
   });
 };
